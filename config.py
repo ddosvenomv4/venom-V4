a0e48c594105cd7c79433aa75cd8a41bd4098b23
ddosvenomv4 <githubddosvenom@gmail.com>
2024-07-17 17:50:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6891694348:AAE0DloNhwgmIk28UMG0RJL2F8Dq6B2UQmk")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("venom24x7")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
-
-
-
-
-
+ADMIN_IDS=("5494398522")
 
 
   
